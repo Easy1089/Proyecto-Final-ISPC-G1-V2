58636fcedec3bdc8395ac3e96e0b46f84d4850c7
MateoAntunez7 <mateoantunez0@gmail.com>
2023-04-29 22:29:53 -0300

    componente login creacion

--- Front-end/Template/src/app/app-routing.module.ts
+++ Front-end/Template/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
   {path:'index',component: IndexComponent},
+  {path:'login',component: IndexComponent},
 ];
 
 @NgModule({
--- Front-end/Template/src/app/app.module.ts
+++ Front-end/Template/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IndexComponent
+    IndexComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
