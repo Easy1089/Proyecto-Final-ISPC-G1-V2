c1bd33bcd9c13a1fe8d754b5632281258a95db68
MateoAntunez7 <mateoantunez0@gmail.com>
2023-04-29 22:23:46 -0300

    Componente index

--- Front-end/Template/src/app/app-routing.module.ts
+++ Front-end/Template/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
+  {path:'index',component: IndexComponent},
 ];
 
 @NgModule({
