a0dee358a2ecef57be8e07099275fc98f7729674
MateoAntunez7 <mateoantunez0@gmail.com>
2023-04-30 13:01:32 -0300

    Componente register y actualizacion login

--- Front-end/Template/src/app/app-routing.module.ts
+++ Front-end/Template/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
   {path:'',component: HomeComponent},
   {path:'index',component: IndexComponent},
-  {path:'login',component: IndexComponent},
+  {path:'login',component: LoginComponent},
+  {path:'register',component: RegisterComponent}
 ];
 
 @NgModule({
