d00394c887793a3e8d91d72b2833b8235de22b2e
Salvador Berón <salvadorberon@gmail.com>
2023-05-01 17:30:03 -0300

    contenido index

--- Front-end/Template/src/app/components/index/index.component.ts
+++ Front-end/Template/src/app/components/index/index.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styles: [
-  ]
+  styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
 
