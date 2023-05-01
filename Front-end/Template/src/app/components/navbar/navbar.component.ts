9054f343c2f42078ced1eb9defd0ad7d8c6be6cd
MateoAntunez7 <mateoantunez0@gmail.com>
2023-05-01 18:31:06 -0300

    Actualizacion navbar login y register

--- Front-end/Template/src/app/components/navbar/navbar.component.ts
+++ Front-end/Template/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
+  register(){
+    this.router.navigate(['/register'])
   }
 
 }
