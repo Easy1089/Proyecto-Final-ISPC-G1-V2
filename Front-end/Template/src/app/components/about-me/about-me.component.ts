11b2a0a4f58c514f0ab205dc36555ae613e6b2d0
marceapaz <TIPSALUD\melisa.apaz>
2023-05-05 11:21:08 -0300

    Agregado de seccion NUESTRO EQUIPO

--- Front-end/Template/src/app/components/about-me/about-me.component.ts
+++ Front-end/Template/src/app/components/about-me/about-me.component.ts
@@ -12,4 +12,55 @@ export class AboutMeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  miembros = [
+    { 
+      name: 'Melisa Apaz',
+      description: 'melisaapaz@gmail.com',
+      image: 'assets/img/miembros/MelisApaz.jpeg'
+    },
+    {
+      name: 'Mateo Antunez',
+      description: 'mateoantunez0@gmail.com',
+      image: 'assets/img/miembros/MateoAntunes.jpg'
+    },
+    {
+      name: 'Salvador Berón',
+      description: 'salvadorberon@gmail.com',
+      image: 'assets/img/miembros/SalvadorBeron.jpg'
+    },
+    {
+      name: 'Ana Eugenia Abregú',
+      description: 'euge.abregu.ea@gmail.com',
+      image: ''
+    },
+    {
+      name: 'Dalma Ponce',
+      description: 'p.d.f.1915@gmail.com',
+      image: ''
+    },
+    {
+      name: 'Jesica  Amaya',
+      description: 'Jesicaeamaya@gmail.com ',
+      image: ''
+    }, 
+    {
+      name: 'Fabiola Benítez',
+      description: 'fabiolaben03@gmail.com',
+      image: ''
+    }, 
+    {
+      name: 'Marcelo Apaz',
+      description: 'marceysoleapazvidela@gmail.com',
+      image: ''
+    }
+  ];
+
+  addToCart(miembro: any) {
+    // implement the add to cart functionality here
+  }
+
+  selectedMiembro: any;
+  selectMiembro(miembro: any) {
+    this.selectedMiembro = miembro;
+  }
 }
