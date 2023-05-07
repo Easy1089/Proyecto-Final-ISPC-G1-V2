d81e8d4bce85445fd1703796df2e5da0e0557f29
MateoAntunez7 <mateoantunez0@gmail.com>
2023-05-07 19:37:09 -0300

    Fix contactos index y carruto

--- Front-end/Template/src/app/components/index/index.component.ts
+++ Front-end/Template/src/app/components/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  contactos() {
+    this.router.navigate(['/contacto'])
+  }
 }
