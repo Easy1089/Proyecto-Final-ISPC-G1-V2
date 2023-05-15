e7509ae41c857d3b0ff49e60c0538e3c328f30b0
MateoAntunez7 <mateoantunez0@gmail.com>
2023-05-15 19:06:04 -0300

    Binding contacto

--- Front-end/Template/src/app/components/contacto/contacto.component.ts
+++ Front-end/Template/src/app/components/contacto/contacto.component.ts
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  nombre: string = '';
+  correoElectronico: string = '';
+  numeroTelefono: string = '';
+  mensaje: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
+  enviarMensaje() {
+    // Aquí puedes agregar la lógica para enviar el mensaje al servidor
+    console.log('Mensaje enviado');
+    console.log('Nombre:', this.nombre);
+    console.log('Correo electrónico:', this.correoElectronico);
+    console.log('Número de teléfono:', this.numeroTelefono);
+    console.log('Mensaje:', this.mensaje);
+  }
 }
