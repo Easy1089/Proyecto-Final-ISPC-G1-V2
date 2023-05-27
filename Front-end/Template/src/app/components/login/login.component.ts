50ed2c6182aab4a4ef7708b992c59ff3be768156
MateoAntunez7 <mateoantunez0@gmail.com>
2023-05-27 15:34:05 -0300

    Formularios reactivos login y register

--- Front-end/Template/src/app/components/login/login.component.ts
+++ Front-end/Template/src/app/components/login/login.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor(private router:Router) { }
+  constructor(private formBuilder: FormBuilder, 
+    private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Aquí puedes agregar la lógica para enviar los datos del formulario
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      console.log('Username:', username);
+      console.log('Password:', password);
+
+      // Aquí puedes llamar a un servicio para enviar los datos del formulario
+      // this.authService.login(username, password);
+    }
   }
 
   register(){
     this.router.navigate(['/register'])
   }
-
 }
--- Front-end/Template/src/app/components/register/register.component.ts
+++ Front-end/Template/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      agreeTerms: [false, Validators.requiredTrue]
+    });
   }
 
+  register(): void {
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    // Aquí puedes implementar la lógica para registrar al usuario
+    // utilizando los valores del formulario (this.registrationForm.value)
+  }
 }
+
+  
