386167e7db9bdde04616d7820416d2caabd5bdd7
MateoAntunez7 <mateoantunez0@gmail.com>
2023-06-07 17:50:52 -0300

    Perfil de usuario reactivo y con sus validaciones

--- Front-end/Template/src/app/components/usuario-profile/usuario-profile.component.ts
+++ Front-end/Template/src/app/components/usuario-profile/usuario-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario-profile',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-profile.component.css']
 })
 export class UsuarioProfileComponent implements OnInit {
+  accountForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.accountForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      orgName: [''],
+      location: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      birthday: ['', Validators.required]
+    });
   }
 
+  saveChanges(): void {
+    // Lógica para guardar los cambios del formulario
+  }
 }
