55fe2763b13ec4e89e427a2dad1724308bdb6a3d
MateoAntunez7 <mateoantunez0@gmail.com>
2023-06-13 16:05:17 -0300

    Fix navbar

--- Front-end/Template/src/app/components/navbar/navbar.component.ts
+++ Front-end/Template/src/app/components/navbar/navbar.component.ts
@@ -5,21 +5,15 @@ import { AuthService } from 'src/app/service/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
-  template: `
-          <div *ngIf="isLoggedIn"> Bienvenido, {{ currentUser.username }}</div>`
-  })
-
+  styleUrls: ['./navbar.component.css']
+})
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
   currentUser: any;
   esAdmin: boolean = false;
   usuarioLogueado: boolean = false;
 
-  constructor(private router: Router, private authService: AuthService) { 
-    this.isLoggedIn = false;
-    this.currentUser = null;
-  }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.authService.currentUser !== null;
@@ -29,30 +23,43 @@ export class NavbarComponent implements OnInit {
     });
 
     this.currentUser = this.authService.currentUser;
-    //this.usuarioLogueado = this.currentUser.username !== undefined;
+    // this.usuarioLogueado = this.currentUser.username !== undefined;
   }
-  
-  inicio(){
-    this.router.navigate([""])
+
+  inicio() {
+    this.router.navigate([""]);
   }
 
   login() {
-    this.router.navigate(['/login'])
+    this.router.navigate(['/login']);
   }
+
   register() {
-    this.router.navigate(['/register'])
+    this.router.navigate(['/register']);
   }
+
   catalogo() {
-    this.router.navigate(['/catalogo'])
+    this.router.navigate(['/catalogo']);
   }
+
   contacto() {
-    this.router.navigate(['/contacto'])
+    this.router.navigate(['/contacto']);
   }
 
   aboutme() {
-    this.router.navigate(['/about-me'])
+    this.router.navigate(['/about-me']);
   }
+
   carrito() {
-    this.router.navigate(['/checkout'])
+    this.router.navigate(['/checkout']);
+  }
+  profile() {
+    this.router.navigate(['/usuarioprofile']);
+  }
+
+  logout() {
+    // Lógica para cerrar sesión
+    this.isLoggedIn = false;
+    this.currentUser = null;
   }
 }
